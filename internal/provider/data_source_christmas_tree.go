76892bee415f608eed2eb01e84cba64d546f695e
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2020-12-10 02:05:38 +0900

    add datasource

--- internal/provider/data_source_christmas_tree.go
+++ internal/provider/data_source_christmas_tree.go
@@ -2,6 +2,8 @@ package provider
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -12,20 +14,39 @@ func dataSourceChristmasTree() *schema.Resource {
 		ReadContext: dataSourceChristmasTreeRead,
 
 		Schema: map[string]*schema.Schema{
-			"sample_attribute": {
+			"path": {
 				Type:     schema.TypeString,
 				Required: true,
+			},
+			"tree": {
+				Type:     schema.TypeString,
+				Computed: true,
+				Optional: true,
 			},
 		},
 	}
 }
 
 func dataSourceChristmasTreeRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// use the meta value to retrieve your client from the provider configure method
-	// client := meta.(*apiClient)
+	var diags diag.Diagnostics
 
-	idFromAPI := "my-id"
-	d.SetId(idFromAPI)
-
-	return diag.Errorf("not implemented")
+	p := d.Get("path").(string)
+	f, err := os.Open(p)
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  err.Error(),
+		})
+	}
+	defer f.Close()
+	tree, err := ioutil.ReadAll(f)
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  err.Error(),
+		})
+	}
+	d.SetId(p)
+	d.Set("tree", string(tree))
+	return diags
 }
